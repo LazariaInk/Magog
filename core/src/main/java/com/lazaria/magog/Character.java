216bd0efa7b019dc5fbe7079095e9ebb5693b025
LazariaInk <lorienlored@gmail.com>
2024-09-24 15:10:11 +0300

    create class for character

---
+++ core/src/main/java/com/lazaria/magog/Character.java
@@ -0,0 +1,158 @@
+package com.lazaria.magog;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+
+public class Character {
+    private Texture idleTexture;
+    private Animation<TextureRegion> idleAnimation;
+    private Texture runTexture;
+    private Animation<TextureRegion> runAnimation;
+    private float x;
+    private float y;
+    private float speed;
+    private boolean movingRight = false;
+    private boolean movingLeft = false;
+    private boolean facingRight = true;
+    private float elapsedTime = 0f;
+    private Sound runSound;
+    private long runSoundId = -1;
+
+    private Character(Builder builder) {
+        this.x = builder.x;
+        this.y = builder.y;
+        this.speed = builder.speed;
+
+        this.idleTexture = builder.idleTexture;
+        TextureRegion[][] tmpIdleFrames = TextureRegion.split(idleTexture, idleTexture.getWidth() / 2, idleTexture.getHeight());
+        Array<TextureRegion> idleFrames = new Array<>();
+        for (TextureRegion[] row : tmpIdleFrames) {
+            for (TextureRegion region : row) {
+                idleFrames.add(region);
+            }
+        }
+        this.idleAnimation = new Animation<>(0.3f, idleFrames, Animation.PlayMode.LOOP);
+
+        this.runTexture = builder.runTexture;
+        TextureRegion[][] tmpRunFrames = TextureRegion.split(runTexture, runTexture.getWidth() / 7, runTexture.getHeight());
+        Array<TextureRegion> runFrames = new Array<>();
+        for (TextureRegion[] row : tmpRunFrames) {
+            for (TextureRegion region : row) {
+                runFrames.add(region);
+            }
+        }
+        this.runAnimation = new Animation<>(0.1f, runFrames, Animation.PlayMode.LOOP);
+
+        this.runSound = builder.runSound;
+    }
+
+    public void update(float delta) {
+        elapsedTime += delta;
+        movingLeft = Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A);
+        movingRight = Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D);
+        if (movingRight) {
+            x += speed * delta;
+            facingRight = true;
+            playRunSound();
+        } else if (movingLeft) {
+            x -= speed * delta;
+            facingRight = false;
+            playRunSound();
+        } else {
+            stopRunSound();
+        }
+    }
+
+    private void playRunSound() {
+        if (runSound != null && runSoundId == -1) {
+            runSoundId = runSound.loop(0.1f);
+        }
+    }
+
+    private void stopRunSound() {
+        if (runSoundId != -1) {
+            runSound.stop(runSoundId);
+            runSoundId = -1;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        Animation<TextureRegion> currentAnimation;
+        if (movingLeft || movingRight) {
+            currentAnimation = runAnimation;
+        } else {
+            currentAnimation = idleAnimation;
+        }
+        TextureRegion currentFrame = currentAnimation.getKeyFrame(elapsedTime, true);
+        if (facingRight && currentFrame.isFlipX()) {
+            currentFrame.flip(true, false);
+        } else if (!facingRight && !currentFrame.isFlipX()) {
+            currentFrame.flip(true, false);
+        }
+        float scaleFactor = 4.0f;
+        float knightWidth = currentFrame.getRegionWidth() * scaleFactor;
+        float knightHeight = currentFrame.getRegionHeight() * scaleFactor;
+
+        batch.draw(currentFrame, x, y, knightWidth, knightHeight);
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void dispose() {
+        idleTexture.dispose();
+        runTexture.dispose();
+        if (runSound != null) {
+            runSound.dispose();
+        }
+    }
+
+    public static class Builder {
+        private Texture idleTexture;
+        private Texture runTexture;
+        private Sound runSound;
+        private float x = 0;
+        private float y = 0;
+        private float speed = 600f;
+
+        public Builder setIdleTexture(Texture idleTexture) {
+            this.idleTexture = idleTexture;
+            return this;
+        }
+
+        public Builder setRunTexture(Texture runTexture) {
+            this.runTexture = runTexture;
+            return this;
+        }
+
+        public Builder setRunSound(Sound runSound) {
+            this.runSound = runSound;
+            return this;
+        }
+
+        public Builder setPosition(float x, float y) {
+            this.x = x;
+            this.y = y;
+            return this;
+        }
+
+        public Builder setSpeed(float speed) {
+            this.speed = speed;
+            return this;
+        }
+
+        public Character build() {
+            if (idleTexture == null || runTexture == null) {
+                throw new IllegalStateException("Idle and Run textures must be set before building Character.");
+            }
+            return new Character(this);
+        }
+    }
+}
--- core/src/main/java/com/lazaria/magog/screen/menu/GameScreen.java
+++ core/src/main/java/com/lazaria/magog/screen/menu/GameScreen.java
@@ -1,138 +1,57 @@
 package com.lazaria.magog.screen.menu;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.ui.Container;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.lazaria.magog.StartGame;
 import com.lazaria.magog.audio.SoundManager;
-import com.lazaria.magog.utils.ButtonFactory;
+import com.lazaria.magog.Character;
 
 public class GameScreen extends ScreenAdapter {
     private Stage stage;
     private FitViewport viewport;
-    ;
-    private StartGame game;
     private SpriteBatch batch;
     private Texture backgroundTexture;
-    private Texture knightTexture;
-    private Animation<TextureRegion> knightAnimation;
-    private float elapsedTime = 0f;
-    private float knightX = 500;
-    private float knightY = 200;
-    private float knightSpeed = 600f;
-    private boolean movingRight = false;
-    private boolean movingLeft = false;
-    private boolean facingRight = true;
-    private Texture idleTexture;
-    private Animation<TextureRegion> idleAnimation;
-    private Container<ImageButton> returnContainer;
-    private Actor fadeActor;
-    private Sound runSound;
-    private long runSoundId = -1;
+    private Character knight;
     private SoundManager soundManager;
-    private ButtonFactory buttonFactory;
 
     public GameScreen(StartGame game) {
         this.game = game;
-        buttonFactory = new ButtonFactory();
-        soundManager = game.getSoundManager();
-        soundManager.playGameMusic();
         viewport = new FitViewport(1920, 1080);
         stage = new Stage(viewport);
         Gdx.input.setInputProcessor(stage);
-        fadeActor = new Actor();
-        fadeActor.setColor(1f, 1f, 1f, 0f);
-        fadeActor.setSize(1920, 1080);
-        stage.addActor(fadeActor);
+
         batch = new SpriteBatch();
         backgroundTexture = new Texture(Gdx.files.internal("firstMap.png"));
-        idleTexture = new Texture(Gdx.files.internal("idle.png"));
-        TextureRegion[][] tmpIdleFrames = TextureRegion.split(idleTexture, idleTexture.getWidth() / 2, idleTexture.getHeight());
-        Array<TextureRegion> idleFrames = new Array<>();
-        for (TextureRegion[] row : tmpIdleFrames) {
-            for (TextureRegion region : row) {
-                idleFrames.add(region);
-            }
-        }
-        idleAnimation = new Animation<>(0.3f, idleFrames, Animation.PlayMode.LOOP);
-        knightTexture = new Texture(Gdx.files.internal("run.png"));
-        TextureRegion[][] tmpFrames = TextureRegion.split(knightTexture, knightTexture.getWidth() / 7, knightTexture.getHeight());
-        Array<TextureRegion> knightFrames = new Array<>();
-        for (TextureRegion[] row : tmpFrames) {
-            for (TextureRegion region : row) {
-                knightFrames.add(region);
-            }
-        }
-        knightAnimation = new Animation<>(0.1f, knightFrames, Animation.PlayMode.LOOP);
-        runSound = Gdx.audio.newSound(Gdx.files.internal("run.wav"));
 
-        returnContainer = buttonFactory.createButton("return.png", 200, 100, viewport.getWorldWidth()
-            - 200 - 20, viewport.getWorldHeight() - 100 - 20, MainMenuScreen.class, stage);
+        knight = new Character.Builder()
+            .setIdleTexture(new Texture(Gdx.files.internal("idle.png")))
+            .setRunTexture(new Texture(Gdx.files.internal("run.png")))
+            .setRunSound(Gdx.audio.newSound(Gdx.files.internal("run.wav")))
+            .setPosition(500, 50)
+            .setSpeed(600f)
+            .build();
 
-
-        stage.addActor(returnContainer);
+        soundManager = game.getSoundManager();
+        soundManager.playGameMusic();
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.setProjectionMatrix(viewport.getCamera().combined);
-        elapsedTime += delta;
-        movingLeft = Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A);
-        movingRight = Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D);
-        if (movingLeft || movingRight) {
-            if (runSoundId == -1) {
-                runSoundId = runSound.loop(0.1f);
-            }
-        } else {
-            if (runSoundId != -1) {
-                runSound.stop(runSoundId);
-                runSoundId = -1;
-            }
-        }
-        if (movingRight) {
-            knightX += knightSpeed * delta;
-            facingRight = true;
-        } else if (movingLeft) {
-            knightX -= knightSpeed * delta;
-            facingRight = false;
-        }
-        Animation<TextureRegion> currentAnimation;
-        if (movingLeft || movingRight) {
-            currentAnimation = knightAnimation;
-        } else {
-            currentAnimation = idleAnimation;
-        }
-        TextureRegion currentFrame = currentAnimation.getKeyFrame(elapsedTime, true);
-        if (facingRight && currentFrame.isFlipX()) {
-            currentFrame.flip(true, false);
-        } else if (!facingRight && !currentFrame.isFlipX()) {
-            currentFrame.flip(true, false);
-        }
-        float scaleFactor = 4.0f;
-        float knightWidth = currentFrame.getRegionWidth() * scaleFactor;
-        float knightHeight = currentFrame.getRegionHeight() * scaleFactor;
-        knightY = 50;
+
+        knight.update(delta);
+
         batch.begin();
         batch.draw(backgroundTexture, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
-        batch.draw(currentFrame, knightX, knightY, knightWidth, knightHeight);
+        knight.render(batch);
         batch.end();
+
         stage.act(delta);
         stage.draw();
     }
@@ -146,8 +65,7 @@ public class GameScreen extends ScreenAdapter {
     public void dispose() {
         batch.dispose();
         backgroundTexture.dispose();
-        knightTexture.dispose();
-        idleTexture.dispose();
+        knight.dispose();
         stage.dispose();
     }
 }
